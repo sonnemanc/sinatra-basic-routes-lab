d4e3242245c71ed2ec087ee6dfe61a5c28483f36
Corban <sonnemanc@gmail.com>
2021-11-17 09:36:45 -0700

    commit tree c48ef904cdabc09a848fe368ce5bd7238c7aa58b

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/name/)
+        resp.write "My name is Tom"
+        resp.status = 200
+    elsif req.path.match(/hometown/)
+        resp.write "My hometown is __"
+        resp.status = 200
+    elsif req.path.match(/favorite-song/)
+        resp.write "My favorite song is "
+        resp.status = 200
+    else
+        resp.write "Route not found"
+        resp.status = 404
+    end
+    resp.finish
+  end
 end
